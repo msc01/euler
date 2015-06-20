6917c31cdc8ab54018856c7004bd861d0d7f1ffa
Michael Schwarze <michael@schwarze-web.de>
2015-06-20 20:06:14 +0200

    Euler003: FUNKTIONIERT

--- euler003.js
+++ euler003.js
@@ -1,14 +1,16 @@
 /** Lösung der Problemstellung Euler#003 (https://projecteuler.net/problem=3):
 	* => Größter Primfaktor der Vorgabezahl 600.851.475.143; testweise von 13.195 (=5, 7, 13 und 29).
   * Herangehensweise: Ermittlung der (größten) Teiler der Vorgabezahl, also von "hinten", um die Anzahl der
-  *   Teilerermittlungen zu reduzieren. Dann Prüfung des jeweiligen Teilers, ob es eine Primzahl ist.
+  *   Teilerermittlungen zu reduzieren. Es müssen nur die Teiler ab/bis zur Wurzel der Vorgabezahl gesucht werden!
+  *   Dann Prüfung des jeweiligen Teilers, ob es eine Primzahl ist.
   *   Im Positivfall kann beendet werden, im Negativfall wird mit der nächsten Zahl weiter gemacht.
 **/
 
 /* ---
  * Variablen
 **/
-var ende = 6851475143; // Zahl, bis zu der gesucht werden soll (600851475143 bzw. 13195)
+var vorgabeZahl = 600851475143; // Zahl, bis zu der gesucht werden soll
+var schranke = Math.round(Math.sqrt(vorgabeZahl)); // Es müssen nur die Teiler ab/bis zur Wurzel der Vorgabezahl gesucht werden!
 var i = 0; // Zähler
 
 /* ---
@@ -25,25 +27,16 @@ var zahlIstPrimzahl = function (zahl) {
 	return true;
 };
 
-// Funktion display zur Anzeige von Texten im Paragraphen mit der ID "ausgabe" des HTML-Dokuments und parallel über die Console
-var display = function(text) {
-  var ausgabeParagraph = document.getElementById('ausgabe');
-	ausgabeParagraph.innerHTML = ausgabeParagraph.innerHTML + text + "<br>";
-	console.log(text);
-	return;
-};
-
-
 /* ---
  * Hauptteil
 **/
 
-i = ende-1;
+i = schranke;
 while (i > 1) {
-	if (ende % i === 0) {
-		display(i + " ist ein Teiler von " + ende);
+	if (vorgabeZahl % i === 0) {
+		display(i + " ist ein Teiler von " + vorgabeZahl);
 	  if (zahlIstPrimzahl(i)) {
-	    display("...und eine Primzahl; somit ist " + i + " der größte Primfaktor von " + ende);
+	    display("...und eine Primzahl!");
 			i=0;
 	  };
 	};
